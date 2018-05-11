6b51c3aeba9b4f0cf10347158054965f688691a7
vizzaccaro <phil@netapplications.com>
2018-05-11 09:04:56 -0700

    updating restify client

--- lib/client.ts
+++ lib/client.ts
@@ -1,15 +1,13 @@
 import utils = require("ns8-utils");
 import restify = require("restify");
-import clients = require("restify-clients");
 
 //  The global restify client for the API calls
 
-let client: restify.Client = clients.createJsonClient(<any>{
+let client: restify.Client = restify.createJsonClient({
     url: utils.config.settings()['apiUrl'],
     version: '*',
     connectTimeout: 5000,
-    requestTimeout: 60000 * 5,
-    agent: false
+    requestTimeout: 60000 * 5
 });
 
 export = client;
