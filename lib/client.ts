1383eeca2c0d8f490dba1376e766a8fa68d6725b
vizzaccaro <phil@netapplications.com>
2018-05-10 17:27:27 -0700

    socket hang ups

--- lib/client.ts
+++ lib/client.ts
@@ -3,11 +3,12 @@ import restify = require("restify");
 
 //  The global restify client for the API calls
 
-let client: restify.Client = restify.createJsonClient({
+let client: restify.Client = restify.createJsonClient(<any>{
     url: utils.config.settings()['apiUrl'],
     version: '*',
     connectTimeout: 5000,
     requestTimeout: 60000 * 5,
+    agent: false,
     retry: {
         'retries': 5
     },
